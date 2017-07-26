943e2554d9ce3be212c94bd7ef29860f0b5c6602
bit-user <princeps90@gmail.com>
2017-07-26 17:58:13 +0900

    done

--- app/src/main/java/com/jx372/gugudanfighter/ResultActivity.java
+++ app/src/main/java/com/jx372/gugudanfighter/ResultActivity.java
@@ -1,7 +1,11 @@
 package com.jx372.gugudanfighter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class ResultActivity extends AppCompatActivity {
@@ -10,5 +14,25 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        Intent intent = getIntent();
+        int score = intent.getIntExtra("correctAnswer",0);
+        int total = intent.getIntExtra("totalQuestion",0);
+
+        ((TextView)findViewById(R.id.correctAnswer)).setText("결과는\n"+score+"/"+total);
+
+        findViewById(R.id.yesBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                startActivity(new Intent(ResultActivity.this,GameActivity.class));
+            }
+        });
+        findViewById(R.id.noBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
